6896060e8192bc15b71460656a00cf03afa99f75
bolt.rstat.org <hello@rstat.org>
2018-12-18 09:50:49 +0000

    new v

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='userlib',
-    version='0.2.0',
+    version='0.3.0',
     description='Rockstat user library',
     url='http://rock.st',
 
