fb47bdcd7d7b899f5710c78790feac4a06470723
Dima <hello@rstat.org>
2018-12-22 09:11:05 +0000

    auth-storee

--- userlib/__init__.py
+++ userlib/__init__.py
@@ -1,5 +1,6 @@
 from .redis import Redis
+from .auth_store import AuthStoreRedis
 
 
-def create_redis(*args, **kwargs):
+def create_redis(*args, **kwargs) -> Redis:
     return Redis(*args, **kwargs)
---
+++ userlib/auth_store/redis.py
@@ -0,0 +1,18 @@
+import ujson
+
+
+class AuthStoreRedis:
+    def __init__(self, redis, key):
+        self.redis = redis
+        self.key = key
+
+    async def set(self, data):
+        await self.redis.set(self.key, ujson.dumps(data))
+
+    async def get(self):
+        data = await self.redis.get(self.key)
+        if data:
+            return ujson.loads(data)
+
+    async def delete(self):
+        await self.redis.delete(self.key)
